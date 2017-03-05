d6c647f1e2e5ba374f506d403774c5979c2fafef
dashingshakeel <shkeelrauf@gmail.com>
2017-03-05 11:55:38 +0500

    User form and profile pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :name, presence: true
   validates :username, uniqueness: true, presence: true
-  validates :email, uniqueness: true, presence: true, format: { with: /^[\w.+-]+@([\w]+.)+\w+$/ }
+  validates :email, uniqueness: true, presence: true, format: { with: /@/ }
   
   before_validation :prep_email
   before_save :create_avatar_url
