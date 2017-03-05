60f43b8e2462a8e79498881b16c5c8d28fb1824f
dashingshakeel <shkeelrauf@gmail.com>
2017-03-05 17:22:28 +0500

    Deployed to heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,11 +34,11 @@ class UsersController < ApplicationController
   end
   def buddies
     current_user = User.find(session[:user_id]) if session[:user_id]
-    
+
     if current_user
         @ribbit = Ribbit.new
         buddies_ids = current_user.followeds.map(&:id).push(current_user.id)
-        @ribbits = Ribbit.find_all_by_user_id buddies_ids
+        @ribbits = Ribbit.where(id: buddies_ids)
     else
         redirect_to root_url
     end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170305075147) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "relationships", force: :cascade do |t|
     t.integer  "follower_id"
     t.integer  "followed_id"
